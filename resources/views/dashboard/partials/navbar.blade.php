183b72b917b1260a2672f72431f95ace0e926dc7
gede sutrisna <gedesutrisna9@gamil.com>
2023-10-04 10:00:15 +0800

    coba1

---
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -0,0 +1,24 @@
+<header class="navbar flex-md-nowrap py-3 px-4">
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <h1 class="page-title">Dashboard</h1>
+    <div class="right d-flex align-items-center gap-4">
+        <div class="">
+            <input class="search-bar" type="text" placeholder="Search" aria-label="Search">
+        </div>
+        <div class="bell position-relative rounded-circle">
+            <div class="dot rounded-circle position-absolute"></div>
+            <img src="/assets/bell-i.svg" alt="">
+        </div>
+        <div class="d-flex gap-3 align-items-center">
+            <div class="profile">
+                <img src="/assets/profile-main.svg" alt="">
+            </div>
+            <div class="">
+                <h1 class="name">Jhon Doe</h1>
+                <p class="job">Inventory Manager</p>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -0,0 +1,42 @@
+            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block sidebar collapse position-fixed nav-custom d-flex justify-content-center align-items-center">
+                <div class="py-3 px-2 sidebar-sticky sidebar custom d-flex flex-column mx-auto">
+                    <a href="/" class="text-decoration-none mx-auto">
+                        <h1 class="name mt-2 mb-5"><img src="/assets/icon.svg" alt="">Store</h1>
+                    </a>
+                <ul class="nav flex-column gap-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/index.html">
+                            <img class="me-2" src="/assets/home-i-active.svg" alt="">
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="order.html">
+                            <img class="me-2" src="/assets/order-i.svg" alt="">
+                            Order
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/product.html">
+                            <img class="me-2" src="/assets/product-i.svg" alt="">
+                            Product
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/promo.html">
+                            <img class="me-2" src="/assets/promo-i.svg" alt="">
+                            Promo
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/component.html">
+                            <img class="me-2" src="/assets/component-i.svg" alt="">
+                            Component
+                        </a>
+                    </li>
+                </ul>
+                <a href="/" class="text-decoration-none mt-auto mx-auto">
+                    <h1 class="nav-link"><img src="/assets/logout-i.svg" alt=""> Log Out</h1>
+                </a>
+                </div>
+            </nav>
