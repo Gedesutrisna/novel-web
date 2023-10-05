4d23f963a78010b159b9f1641966ddb073296157
agus <agusputraa47@gmail.com>
2023-10-05 11:29:06 +0800

    main

--- app/Http/Controllers/NovelController.php
+++ app/Http/Controllers/NovelController.php
@@ -11,9 +11,9 @@ class NovelController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function novel()
     {
-        //
+        return view('dashboard.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\NovelController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::get('/dashborad', function () {
-    return view('welcome');
-});
+Route::get('/novel', [NovelController::class, 'novel'])->name('novel');
