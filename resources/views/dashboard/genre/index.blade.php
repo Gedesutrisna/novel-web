e719c8321e9c0044a39fad0a05afa04cdf69e534
agus <agusputraa47@gmail.com>
2023-10-05 11:24:10 +0800

    main

---
+++ resources/views/dashboard/genre/index.blade.php
@@ -0,0 +1,8 @@
+@extends('dashboard.layouts.main')
+@section('container')
+    <section>
+        <div class="container">
+            <h1>Hello World</h1>
+        </div>
+    </section>
+@endsection
